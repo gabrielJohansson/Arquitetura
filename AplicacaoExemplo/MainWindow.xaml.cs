87b59b685836a044b555010a44f3434c917ccca6
Aluno <Aluno@DOSO125250>
2018-10-02 11:14:25 -0300

    commit

--- AplicacaoExemplo/MainWindow.xaml.cs
+++ AplicacaoExemplo/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
@@ -20,14 +21,37 @@ namespace AplicacaoExemplo
     /// </summary>
     public partial class MainWindow : Window
     {
+       
+
         public MainWindow()
         {
             InitializeComponent();
         }
 
+        private bool NumberValidationTextBox(string Text)
+        {
+            Regex regex = new Regex("[^0-9]+");
+           return regex.IsMatch(Text);
+        }
+
         private void Btn_Click(object sender, RoutedEventArgs e)
         {
-            this.BtnPrincipal.Content = "Olá";
+            //this.BtnPrincipal.Content = "Olá";
+
+            if (NumberValidationTextBox(this.txtDiminuidor.Text) && NumberValidationTextBox(this.txtContador.Text))
+            {
+                this.txtDiminuidor.Text = (Convert.ToInt32(this.txtDiminuidor.Text) - 1).ToString();
+              
+                this.txtContador.Text = (Convert.ToInt32(this.txtContador.Text) + 1).ToString();              
+            }
+            else
+            {
+                this.txtContador.Text = 0.ToString();
+                this.txtDiminuidor.Text = 50.ToString();
+               
+                MessageBox.Show("insira apenas numeros");
+            }
+          
         }
     }
 }
